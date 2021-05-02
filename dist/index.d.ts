f3200f477592e68aaf933ca19fd37cdcabd154d1
S.Koike <shomakoike0909@gmail.com>
2021-05-02 17:49:15 +0900

    feat: build

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -5,8 +5,18 @@ declare class nftTransactionHelper {
     constructor(mnemonicOrPrivateKey: string, url: string);
     setContract(contractAddress: string): void;
     getAddress(): string;
+    getOwner(tokenId: string): Promise<string>;
+    getApproved(tokenId: string): Promise<string>;
     isOwner(tokenId: string): Promise<boolean>;
-    transferToken(tokenId: string, toAddress: string): Promise<string>;
+    isApproved(tokenId: string): Promise<boolean>;
+    getBalance(): Promise<number>;
+    transfer(toAddress: string, tokenId: string): Promise<string>;
+    approve(toAddress: string, tokenId: string): Promise<string>;
+    transferFrom(fromAddress: string, toAddress: string, tokenId: string, option?: {
+        useSafeTransferFrom: boolean;
+    }): Promise<string>;
+    setApprovalForAll(operator: string): Promise<string>;
+    isApprovedForAll(operator: string): Promise<boolean>;
     end(): void;
     private isInitialized;
 }
--- dist/index.js
+++ dist/index.js
@@ -1,31 +1,11 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const hdwallet_provider_1 = __importDefault(require("@truffle/hdwallet-provider"));
 const web3_1 = __importDefault(require("web3"));
-const fs = __importStar(require("fs"));
-const erc721abi = JSON.parse(fs.readFileSync('./lib/abi.json', 'UTF-8'));
+const abi_1 = __importDefault(require("./lib/abi"));
 class nftTransactionHelper {
     constructor(mnemonicOrPrivateKey, url) {
         this.client = new web3_1.default();
@@ -34,23 +14,77 @@ class nftTransactionHelper {
         this.provider = provider;
     }
     setContract(contractAddress) {
-        this.contract = new this.client.eth.Contract(erc721abi, contractAddress);
+        this.contract = new this.client.eth.Contract(abi_1.default, contractAddress);
     }
     getAddress() {
         var _a, _b;
         return (_b = (_a = this.provider) === null || _a === void 0 ? void 0 : _a.getAddress()) !== null && _b !== void 0 ? _b : '';
     }
-    async isOwner(tokenId) {
+    async getOwner(tokenId) {
         if (this.isInitialized(this.contract)) {
-            const ownerAddress = await this.contract.methods.ownerOf(tokenId).call();
-            return ownerAddress === this.getAddress();
+            return await this.contract.methods.ownerOf(tokenId).call();
         }
-        return false;
+        throw new Error('Contract not initialized');
     }
-    async transferToken(tokenId, toAddress) {
+    async getApproved(tokenId) {
+        if (this.isInitialized(this.contract)) {
+            const approvedOperator = await this.contract.methods.getApproved(tokenId).call();
+            return String(approvedOperator);
+        }
+        throw new Error('Contract not initialized');
+    }
+    async isOwner(tokenId) {
+        const owner = await this.getOwner(tokenId);
+        return owner === this.getAddress();
+    }
+    async isApproved(tokenId) {
+        const approvedOperator = await this.getApproved(tokenId);
+        return approvedOperator === this.getAddress();
+    }
+    async getBalance() {
+        if (this.isInitialized(this.contract)) {
+            const balance = await this.contract.methods.balanceOf(this.getAddress()).call();
+            return Number(balance);
+        }
+        throw new Error('Contract not initialized');
+    }
+    async transfer(toAddress, tokenId) {
         if (this.isInitialized(this.contract)) {
             const txHash = await this.contract.methods.transfer(tokenId, toAddress).send();
             return String(txHash);
+        }
+        throw new Error('Contract not initialized');
+    }
+    async approve(toAddress, tokenId) {
+        if (this.isInitialized(this.contract)) {
+            const txHash = await this.contract.methods.approve(toAddress, tokenId).send();
+            return String(txHash);
+        }
+        throw new Error('Contract not initialized');
+    }
+    async transferFrom(fromAddress, toAddress, tokenId, option) {
+        if (this.isInitialized(this.contract)) {
+            if (this.isApproved(tokenId))
+                throw new Error('You are not approved to execute transaction');
+            const method = (option === null || option === void 0 ? void 0 : option.useSafeTransferFrom) ? 'safeTransferFrom' : 'transferFrom';
+            const txHash = await this.contract.methods[method](fromAddress, toAddress, tokenId).send();
+            return String(txHash);
+        }
+        throw new Error('Contract not initialized');
+    }
+    async setApprovalForAll(operator) {
+        if (this.isInitialized(this.contract)) {
+            if (this.isApprovedForAll(operator))
+                throw new Error('Already Approved');
+            const txHash = await this.contract.methods.setApprovalForAll(operator, true).send();
+            return String(txHash);
+        }
+        throw new Error('Contract not initialized');
+    }
+    async isApprovedForAll(operator) {
+        if (this.isInitialized(this.contract)) {
+            const isApproved = await this.contract.methods.isApprovedForAll(this.getAddress(), operator).call();
+            return isApproved;
         }
         throw new Error('Contract not initialized');
     }
