c00bd0f676b9a55816cb7d14c092cf99cc6b31b2
S.Koike <shomakoike0909@gmail.com>
2021-05-21 22:26:06 +0900

    feat: check container status

---
+++ test/env/checkStatus.ts
@@ -0,0 +1,27 @@
+import Web3 from 'web3';
+
+const client = new Web3('http://localhost:8545');
+const sleep = (msec: number) => new Promise(resolve => setTimeout(resolve, msec));
+
+async function waitUntilContainerReady(seconds: number): Promise<void> {
+    const promises: Promise<unknown>[] = [];
+    promises.push((async () => {
+        sleep(seconds * 1000);
+        throw new Error('Fial to check test env status');
+    })());
+    promises.push((async () => {
+        while(true) {
+            try {
+                const blockNumber = await client.eth.getBlockNumber();
+                console.log('Prepared. block number: ' + JSON.stringify(blockNumber));
+                return;
+            } catch (error) {
+                console.log('Not prepared yet...');
+                await sleep(1000 * 5);
+            }
+        }
+    })());
+    await Promise.race(promises);
+}
+
+waitUntilContainerReady(30);
